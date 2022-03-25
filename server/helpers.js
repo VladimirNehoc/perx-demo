39d9a580893f3d72669d1628b346e18d59177a31
Vladimir Nehoc <vladimirnehoc@mail.ru>
2022-03-25 12:27:40 +0400

    created feathers server with database

--- server/helpers.js
+++
@@ -1,9 +0,0 @@
-const _ = require('lodash');
-
-module.exports.getWhereString = (str) => {
-  const items = _.split(str, ',');
-
-  const itemsStringArray = _.map(items, (item) => `'${item}'`);
-
-  return _.join(itemsStringArray, ', ');
-};
